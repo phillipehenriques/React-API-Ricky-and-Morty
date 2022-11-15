b34b00664d780cbfa570bdb850e5640588f9cd5e
phillipe henriques <phillipe_ch@hotmail.com>
2022-11-15 17:04:08 -0300

    mfc

--- src/index.js
+++ src/index.js
@@ -3,15 +3,66 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CSSReset } from "./CSSReset";
+import { EstiloDaNavbar } from "./style/EstiloDaNavbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function HomePage() {
+  const mensagem = "";
+  const estilosDaHomePage = {backgroundColor: "blue", width: "100%"};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+      <>
+      <CSSReset />
+      <div style={estilosDaHomePage}>
+          <Navbar/>
+          <Container />
+          <Bottom />
+      </div>
+      </>
+  );
+}
+
+ export default HomePage;
+
+    // funções <<
+
+  const StyledHeader = styled.div` //o que é isso e porque isso está aqui Csorgo? <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  .userInfo {
+      margin-top: 56px;
+      margin-bottom: 20px;
+      color: white;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: start;
+      width: 100%;
+      padding: 16px 32px;
+      gap: 16px;
+  }
+`;
+  
+function Navbar() {
+  return (
+      <EstiloDaNavbar>
+
+      </EstiloDaNavbar>
+  )
+}
+
+function Container() {
+  return (
+      <Container>
+      
+      </Container>
+  )
+}
+
+function Bottom() {
+  return (
+      <Bottom>
+      
+      </Bottom>
+  )
+}
+
+// fim
