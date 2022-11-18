95898faf0d0f134df07c59b7b3fcef256141943d
phillipe henriques <phillipe_ch@hotmail.com>
2022-11-17 21:32:58 -0300

    funcionando

--- src/App.js
+++ src/App.js
@@ -1,66 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import styled from "styled-components";
-import { CSSReset } from "./CSSReset";
-import { EstiloDaNavbar } from "./style/EstiloDaNavbar";
 
 function HomePage() {
-  const mensagem = "";
-  const estilosDaHomePage = {backgroundColor: "blue", width: "100%"};
-
   return (
-      <>
-      <CSSReset />
-      <div style={estilosDaHomePage}>
-          <Navbar/>
-          <Container />
-          <Bottom />
+    <>
+      <div>
+        <Navbar />
       </div>
-      </>
+    </>
   );
 }
 
- export default HomePage;
+export default HomePage;
 
-    // funções <<
+// funções <<
 
-  const StyledHeader = styled.div` // o que é isso e porque isso está aqui Csorgo? <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  .barra {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  background-color: black;
-  color: white;
-  width: 100%;
-  height: 12vh;
-  }
+const StyledNav = styled.div`
+      .barra {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        background-color: black;
+        color: white;
+        width: 100%;
+        height: 12vh;
+      }
 `;
-  
+
 function Navbar() {
   return (
-      <EstiloDaNavbar>
-        <div className="barra">
-          Alguma coisa aqui!
-        </div>
-      </EstiloDaNavbar>
-  )
-}
-
-function Container() {
-  return (
-      <Container>
-      
-      </Container>
-  )
-}
-
-function Bottom() {
-  return (
-      <Bottom>
-      
-      </Bottom>
+    <StyledNav>
+      <div className='barra'>
+        Alguma coisa aqui!
+      </div>
+    </StyledNav>
   )
 }
 
