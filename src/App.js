32d4d02820dc8e168f7224d28d1ddcb532cd356d
phillipe henriques <phillipe_ch@hotmail.com>
2022-11-22 21:20:16 -0300

    primeiros passos no estilo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function HomePage() {
     <>
       <div>
         <Navbar />
+        <Middle />
       </div>
     </>
   );
@@ -32,10 +33,65 @@ function Navbar() {
   return (
     <StyledNav>
       <div className='barra'>
-        Alguma coisa aqui!
+        API Rick & Morty!
       </div>
     </StyledNav>
   )
 }
+
+const StyledMiddle = styled.div`
+  .middleRules {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: lightcoral;
+    margin: auto;
+    font-size: 25px;
+    gap: 15px;
+    width: 80%;
+    height: 80vh;
+  };
+
+  .fotoPersonagem {
+    background-color: orange;
+    width: 200px;
+    height: 200px;
+  };
+
+  .rules {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+  }
+
+`;
+
+function Middle() {
+  return (
+    <StyledMiddle>
+      <div className='middleRules'>
+        <div className='rules'>
+          <div className='fotoPersonagem'>
+            {/* foto */}
+          </div>
+          <div className='dadosPersonagem'>
+            Nome <div className='linhaPersonagem' id='nomePersonagem'></div>
+            Espécie<div className='linhaPersonagem' id='especiePersonagem'></div>
+            Status<div className='linhaPersonagem' id='statusPersonagem'></div>
+            Local<div className='linhaPersonagem' id='localPersonagem'></div>
+          </div>
+        </div>
+        <div>
+          {/* blocos de personagens */}
+        </div>
+      </div>
+    </StyledMiddle>
+  )
+}
+
+
 
 // fim
